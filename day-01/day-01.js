429be835f8e24b1090a14299f8d76a57d2d27a86
Rakib-Hossan <rakib.redoy01@gmail.com>
2023-10-28 11:11:04 +0600

    rules of variable name

--- day-01/day-01.js
+++ day-01/day-01.js
@@ -1,12 +1,24 @@
-// what is variable?
-// -->Variables are Containers for Storing Data
+/*
+what is variable?
+-->Variables are Containers for Storing Data
+*/
+
 // How to declare variable in JavaScript
+
 var name = "Rakib";
-                    // here 'var' is the keyword to define as variable
-                    // 'name' is the variable name
-                    // '=' is use to assign value in variable name
-                    // "Rakib" is the variable value which is set in 'name' variable
-                    // ";" is used to stop the statement 
+
+/*
+ here 'var' is the keyword to define as variable, 'name' is the variable name '=' is use to assign value in variable name, "Rakib" is the variable value which is set in 'name' variable, ";" is used to stop the statement .
+ */
+
+/* Rules of declare variable name
+
+    1. variable name can not be any keywords.
+    2. variable name has to be in one word. No space between variable name.
+    3. variable name can not have quotation.
+    4. variable name can not starts with a number but use number under variable name.
+    5. to write long name of variable we use '_' between variable name. We also used camel case. 
+*/
 
 // Different data type of variable
 
