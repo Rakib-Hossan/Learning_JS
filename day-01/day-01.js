dce0e43c2794534920926b4c6dbdd17f0889b868
Rakib-Hossan <rakib.redoy01@gmail.com>
2023-10-28 10:22:06 +0600

    define variable in js

--- day-01/day-01.js
+++ day-01/day-01.js
@@ -1,1 +1,6 @@
 // How to declare variable in JavaScript
+var name = "Rakib";
+                    // here 'var' is the keyword to define as variable
+                    // 'name' is the variable name
+                    // '=' is use to assign value in variable name
+                    // "Rakib" is the variable value which is set in 'name' variable
