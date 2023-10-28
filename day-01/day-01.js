d40ffc938e6deee4f16e7ac1b8ba192609a2c0ff
Rakib-Hossan <rakib.redoy01@gmail.com>
2023-10-28 10:37:37 +0600

    different data type

--- day-01/day-01.js
+++ day-01/day-01.js
@@ -1,6 +1,20 @@
+// what is variable?
+// -->Variables are Containers for Storing Data
 // How to declare variable in JavaScript
 var name = "Rakib";
                     // here 'var' is the keyword to define as variable
                     // 'name' is the variable name
                     // '=' is use to assign value in variable name
                     // "Rakib" is the variable value which is set in 'name' variable
+                    // ";" is used to stop the statement 
+
+// Different data type of variable
+
+// String data type
+var friend = 'Farhan'; // To write string we wrtie the value into "--" or '--'
+
+// Numeric data type
+var age = 18; 
+
+// Boolean data type
+var isTrue = true; // the boolean data type returns only true or false
