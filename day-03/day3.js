c21c2097bc38810f013cd954e006610298d604a5
Rakib-Hossan <rakib.redoy01@gmail.com>
2023-11-02 10:32:25 +0600

    learning upto while loop

--- day-03/day3.js
+++ day-03/day3.js
@@ -2,3 +2,14 @@
     Loop in js
 =>  Loops can execute a block of code a number of times. if we want to run the same code over and over again, each time with a different value.
 */
+
+// while loop 
+
+var num = 1; //loop variable
+//while loop condition
+while(num<=10){
+    console.log(num);
+    num++; // increasing or decreasing variable value;
+}
+
+// for loop 
