6b492521480b909284a8af11fc2162e7ad5357ec
Rakib-Hossan <rakib.redoy01@gmail.com>
2023-11-03 09:05:31 +0600

    learning upto break and continue statement

--- day-03/day3.js
+++ day-03/day3.js
@@ -17,3 +17,30 @@ while(num<=10){
 for (var i = 1; i <= 10; i++){
     console.log(i);
 }
+
+/*
+    break statement 
+=>  The break statement "jumps out" of a loop.
+*/ 
+// break example 
+for( var i=1; i<=20; i++){
+    console.log("i love you");
+    if(i>5){
+        console.log("Loop are stopped for break statement");
+        break;
+    }
+}
+
+/*
+    continue statement
+=>  The continue statement "jumps over" one iteration in the loop.
+*/
+// continue example 
+var num = [50, 25, 14, 51, 65, 27, 100, 87, 54, 90];
+for (var i=0; i<num.length;i++){
+    var number = num[i];
+    if(number>50){
+        continue;
+    }
+    console.log(number);
+}
